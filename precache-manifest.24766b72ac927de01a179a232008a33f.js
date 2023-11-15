7c50f7aaa694373a691741842f7632c89b2249de
Larry Sprock <lar@Larrys-MacBook-Pro.local>
2023-11-15 10:42:50 -0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.24766b72ac927de01a179a232008a33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00cd5d8937141a5e8c02032c81250e72",
+    "url": "/pspdfkit-demo/index.html"
+  },
+  {
+    "revision": "f1c55abcc07bc841f0a4",
+    "url": "/pspdfkit-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3cddfba8bf177b40ea59",
+    "url": "/pspdfkit-demo/static/js/2.b1ef015a.chunk.js"
+  },
+  {
+    "revision": "26a189333e5d34fed295",
+    "url": "/pspdfkit-demo/static/js/3.722d337c.chunk.js"
+  },
+  {
+    "revision": "a7c13b74067f0c964716",
+    "url": "/pspdfkit-demo/static/js/4.8c8360b9.chunk.js"
+  },
+  {
+    "revision": "f1c55abcc07bc841f0a4",
+    "url": "/pspdfkit-demo/static/js/main.bfe5cd73.chunk.js"
+  },
+  {
+    "revision": "ded8a77e3568d522f7cf",
+    "url": "/pspdfkit-demo/static/js/runtime~main.13c6a630.js"
+  }
+]);
