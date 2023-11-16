46ce781c234b036e90a6a7d6ccfdc229a0cd49c1
Larry Sprock <lar@Larrys-MacBook-Pro.local>
2023-11-16 12:53:28 -0800

    Fix unused var

--- src/components/PdfViewerComponent.js
+++ src/components/PdfViewerComponent.js
@@ -7,14 +7,14 @@ export default function PdfViewerComponent(props) {
   useEffect(() => {
     const container = containerRef.current; // This `useRef` instance will render the PDF.
 
-    let PSPDFKit, instance;
+    let PSPDFKit;
 
     (async function () {
       PSPDFKit = await import("pspdfkit")
 
 		PSPDFKit.unload(container) // Ensure that there's only one PSPDFKit instance.
 
-      instance = await PSPDFKit.load({
+      return await PSPDFKit.load({
         licenseKey: 'skhUNdHevdfv3ocq8Y13s-tlpl-FkkxNnjpYxLQcJNnflz3zS7ostYvNw26Fevam2VRgvpfeSxvY5GwDZIsw4fxt3OhMhE3KSCmsJt_NNAC6Smdpt_HT7bB34N5ZNFwqO2gr5jHfwPRKUA24KWLgusRIZADLA1jLL0GNuZnPfPcu9tpKFZujBQAP9nlP6pVhSYOQ1OQQHQuM6pvnSTUT8xOsekn-OWAU-Cq8buUUtEFixXf_7bxymB9pKDPfltXZIfoxMGnVbC9Ccjo6B-mrbe83i5xG2yxAKux5hKv9O64J2wE9CGJOjWEn4SlmrUwVceOeAQr5ZQga-_YaRryuqP-K2tiliCzZlVLYMY9mCFE7Mo-nATvBW3uC87kyDnk1lyaCOU9l3tOAp4UnPb96eMwygMlspMSbwjUwHFh9ezF_m4THlVK3O2z9qlLEvORaW-8JYeqxJt9wqDKzNiXeIdVUdYrCMhqFm1LvbjjwRQ6pYbklcVHAMJyT4AlZU7i3X9hdQYUmWQkdmgrifqSi8bdanI7yGyMB7ny4EAYGTOJPgI7aVhFOZvMklw_putUWdActQvw6XdRFUufKTYo6--cXrjLSRk1Mr7NPrKqMIm1MqSPub9rWG5NR3apDbo0E2bMKYQ408eoKevfMWhc6mWUKT9ONiqMUTQ7_FiJuBtA=',
         // Container where PSPDFKit should be mounted.
         container,
